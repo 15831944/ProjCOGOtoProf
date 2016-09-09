787fd788a9c8d9e1e184af8e72027d41e9321bba
Владимир Прудников <vladimirsp81@outlook.com>
2016-09-09 16:57:25 +0300

    Пробная версия

---
+++ ProjCOGO to Prof/ProjCOGO to Prof/ProjCOGOtoProf.cs
@@ -0,0 +1,112 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.GraphicsInterface;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using System.Linq;
+
+namespace ProjCOGOtoProf
+{
+    public class PViewCOGOPointCmd
+    {
+        private static PViewCOGOPointDO _overrule = null;
+
+        [CommandMethod("OnOffPViewCogoPoint")]
+        public void Run()
+        {
+            if (_overrule == null)
+            {
+                _overrule = new PViewCOGOPointDO();
+                Overrule.AddOverrule
+                    (RXClass.GetClass(typeof(CogoPoint)), _overrule, false);
+
+            }
+            else
+            {
+                Overrule.RemoveOverrule
+                    (RXClass.GetClass(typeof(CogoPoint)), _overrule);
+                _overrule = null;
+            }
+            Application.DocumentManager.
+                MdiActiveDocument.Editor.Regen();
+        }
+    }
+
+    public class PViewCOGOPointDO : DrawableOverrule
+    {
+        static bool NotExist(ObjectId id)
+        {
+            return id.IsNull
+                || !id.IsValid
+                || id.IsErased
+                || id.IsEffectivelyErased;
+        }
+
+        public override bool WorldDraw(Drawable drawable, WorldDraw wd)
+        {
+            if (drawable is CogoPoint)
+            {
+                CogoPoint cogoPt = drawable as CogoPoint;
+
+                Database db = HostApplicationServices.WorkingDatabase;
+                CivilDocument cdoc = CivilDocument.GetCivilDocument(db);
+                ObjectId[] alignIds = cdoc.GetAlignmentIds().Cast<ObjectId>().ToArray();
+
+                // Проходим по трассам четрежа
+                foreach (ObjectId alignId in alignIds)
+                {
+                    // Если трассы нет
+                    if (NotExist(alignId))
+                        // Переходим к следующей
+                        continue;
+
+                    // Пикетаж и смещение точки
+                    double station = 0.0, offset = 0.0;
+
+                    // Открываем трассу на чтение
+#pragma warning disable 618
+                    using (Alignment align = alignId.Open
+                        (OpenMode.ForRead, false, true) as Alignment)
+#pragma warning restore 618
+                    {
+                        align.StationOffset(cogoPt.Location.X, cogoPt.Location.Y, ref station, ref offset);
+
+                        // Получаем виды профилей трассы
+                        ObjectId[] alignPViewIds = align.GetProfileViewIds().Cast<ObjectId>().ToArray();
+
+                        // проходим по видам профилей
+                        foreach (ObjectId pViewId in alignPViewIds)
+                        {
+                            if (NotExist(pViewId))
+                                continue;
+#pragma warning disable 618
+                            using (ProfileView pView = pViewId.Open
+                                (OpenMode.ForRead, false, true) as ProfileView)
+#pragma warning restore 618
+                            {
+                                double pViewCoordX = 0.0, pViewCoordY = 0.0;
+
+                                // Если тока проецируется на вид профиля
+                                if (pView.FindXYAtStationAndElevation(station, cogoPt.Location.Z, ref pViewCoordX, ref pViewCoordY))
+                                {
+                                    // Рисуем отображение окружности в этой точке
+                                    // Здесь можно отобразить любой объект (кроме маскировки), в том числе и блок
+                                    using (Circle ci = new Circle
+                                        (new Point3d(pViewCoordX, pViewCoordY, pView.Location.Z),
+                                        Vector3d.ZAxis, 2.0))
+                                    {
+                                        ci.WorldDraw(wd);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return base.WorldDraw(drawable, wd);
+        }
+    }
+}
---
+++ ProjCOGO to Prof/ProjCOGO to Prof/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ProjCOGO to Prof")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("ProjCOGO to Prof")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2ff81e11-9721-4dd3-af70-5bfc0af03523")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
